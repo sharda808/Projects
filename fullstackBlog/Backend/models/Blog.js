5e60eb2a2af75f86765e3f93df378c00ca585b19
sharda <shardakumari96115@gmail.com>
2026-04-03 13:01:28 +0530

    backend is complete for fullstakBlog

--- fullstackBlog/Backend/models/Blog.js
+++ fullstackBlog/Backend/models/Blog.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
-const blogSchema = new mongoose.Sechema({
+const blogSchema = new mongoose.Schema({
   title:{type:String, required:true},
   content:{type:String , required:true},
   author:{type:String, required: true},
   createdAt:{type:Date, default: Date.now},
   likes:{type:Number, default:0},
-  commets:[{
+  comments:[{
     username:{type: String, required:true},
     content: {type:String, required: true},
     CreatedAt: {type: Date, default: Date.now},
